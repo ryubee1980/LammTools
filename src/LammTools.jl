69364752bb75a4f7fd2a6c0e4adfe617ff067ad7
ryubee1980 <ryubee@gmail.com>
2025-01-16 14:31:01 +0900

    update

--- src/LammTools.jl
+++ src/LammTools.jl
@@ -1,5 +1,131 @@
 module LammTools
-test
-# Write your package code here.
+#Copyright (c) 2021 Ryuichi Okamoto <ryubee@gmail.com>
+#License: https://opensource.org/licenses/MIT
+
+function ext_dump(file;Tstep=2000000,N=4152)
+    fn=open(file,"r")
+    fw=open("ex_$(Tstep).dump", "w")
+    fw2=open("ex_$(Tstep).data","w")
+
+    println(fw2,"LAMMPS data file via write_data, version 30 Nov 2020, timestep = $(Tstep)")
+    println(fw2,"")
+    println(fw2,"$(N) atoms")
+    println(fw2,"13 atom types")
+    println(fw2,"")
+    println(fw2, "0 30.828 xlo xhi")
+    println(fw2, "0 47.82 ylo yhi")
+    println(fw2, "0 30.828 zlo zhi")
+    println(fw2,"")
+    println(fw2,"Masses")
+
+    println(fw2,"")
+    println(fw2,"1 12")
+    println(fw2,"2 1.008")
+    println(fw2,"3 1.008")
+    println(fw2,"4 16")
+    println(fw2,"5 16")
+    println(fw2,"6 88.90519999999999")
+    println(fw2,"7 91.224")
+    println(fw2,"8 1.008")
+    println(fw2,"9 16")
+    println(fw2,"10 12")
+    println(fw2,"11 1.008")
+    println(fw2,"12 16")
+    println(fw2,"13 1.008")
+
+    println(fw2,"")
+
+    println(fw2,"Atoms # charge")
+
+    println(fw2,"")
+
+
+    
+
+
+
+    line=readline(fn)
+    s=split(line)
+    line2=readline(fn)
+
+    if line=="ITEM: TIMESTEP" && line2=="0"
+        println(fw,line)
+        println(fw, line2)
+    end
+
+    line=readline(fn)
+    while line!="ITEM: TIMESTEP"
+        println(fw,line)
+        line=readline(fn)
+    end
+
+#    while !(line=="ITEM: TIMESTEP" && readline(fn)==Tstep)
+#        line=readline(fn)
+#    end
+
+     while line2!="$Tstep"
+	line=readline(fn)
+        while line!="ITEM: TIMESTEP"
+            line=readline(fn)
+        end
+        line2=readline(fn)
+        println("timestep="*line2)
+    end
+
+    println(fw,"ITEM: TIMESTEP")
+    println(fw,Tstep)
+
+    line=readline(fn)
+    while line!="ITEM: TIMESTEP" && !eof(fn)
+        println(fw,line)
+        line=readline(fn)
+        s=split(line)
+        if length(s)==6 && s[1]!="ITEM:"
+            println(fw2,line)
+        end
+    end    
+
+
+    close(fn)
+    close(fw)
+    close(fw2)
+    
+end
+
+
+function ext_bonds(file;Tstep=30000,N=5717)
+    fn=open(file,"r")
+    
+    fw2=open("bonds_$(Tstep).reaxc","w")
+
+    println(fw2,"# Timestep $(Tstep)")
+    println(fw2,"#")
+    println(fw2,"# Number of particles $(N)")
+    println(fw2,"#")
+    println(fw2,"# Max number of bonds per atom 14 with coarse bond order cutoff 0.300")
+    println(fw2, "# Particle connection table and bond orders")
+    println(fw2, "# id type nb id_1...id_nb mol bo_1...bo_nb abo nlp q")
+    
+    line="initial"
+ 
+    while line!="# Timestep $(Tstep) "
+        line=readline(fn)    
+    end
+    println("line $(Tstep) read")
+
+    for i in 1:7
+        line=readline(fn)
+    end
+
+    while !eof(fn) && line!="# "
+        println(fw2,line)
+        line=readline(fn)
+    end
+
+
+    close(fn)
+    close(fw2)
+    
+end
 
 end
